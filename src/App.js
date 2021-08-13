30e2326e398fec0ad8913495e2e8272be69e3b67
Nielsenprambudi <nielsenprambudiasallie@gmail.com>
2021-08-13 14:03:34 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import AppNavbar from "./component/layout/AppNavbar";
 import Login from "./component/auth/Login";
 import Register from "./component/auth/Register";
 import Home from "./component/layout/Home";
-// import Profile from "./component/layout/Profile";
+import Profile from "./component/layout/Profile";
 import ArticlesPage from "./component/layout/ArticlesPage";
-// import WriterManageArticles from "./component/layout/WriterManageArticles";
-// import PostComment from "./component/layout/PostComment";
-// import ArticleDetail from "./component/layout/ArticleDetail";
-import { Provider, connect } from "react-redux";
-import { addToken } from "./store/actions/ConfigAction";
+import CreateArticle from "./component/layout/CreateArticle";
+import ArticleDetail from "./component/layout/ArticleDetail";
+import { Provider } from "react-redux";
 import ConfigureStore from "./store/ConfigureStore";
 import './App.css';
 
@@ -24,13 +22,13 @@ class App extends Component {
             <AppNavbar/>
             <div className="container">
               <Switch>
-                {/* <Route exact path="/" component={UserIsAuthenticated(ArticlesPage)} /> */}
-                {/* <Route exact path="/client/add" component={AddClient} />
-                <Route exact path="/client/edit/:id" component={UserIsAuthenticated(EditClient)} />
-                <Route exact path="/client/:id" component={UserIsAuthenticated(ClientDetails)} /> */}
-                {/* <Route exact path="/Profile/:id" component={UserIsNotAuthenticated(Profile)} /> */}
                 <Route exact path="/" component={Home} />
+                <Route exact path="/profile" component={Profile} />
                 <Route exact path="/articles" component={ArticlesPage} />
+                <Route exact path="/article/detail/:id" component={ArticleDetail} />
+                <Route exact path="/article/detail/:id/edit" component={CreateArticle} />
+                <Route exact path="/article/post" component={CreateArticle} />
+                <Route exact path="/article/post/:id" component={CreateArticle} />
                 <Route exact path="/login" component={Login} />
                 <Route exact path="/register" component={Register} />
               </Switch>
